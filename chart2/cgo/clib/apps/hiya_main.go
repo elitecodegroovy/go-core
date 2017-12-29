a588c08995c532506c13e1fb95eda129c80aa226
elite_jigang <elite_jigang@163.com>
2017-12-29 22:27:55 +0800

    added

--- chart2/cgo/clib/apps/hiya_main.go
+++ chart2/cgo/clib/apps/hiya_main.go
@@ -1,13 +1,10 @@
 package main
 
-//import "github.com/elitecodegroovy/go-core/chart2/cgo/clib/hiya"
-//
-//package main
-//
-//import "hiya"
-//
-//func main() {
-//	msg := hiya.CreateMessage("Welcome to cgo world!")
-//	hiya.DisplayMessage(msg)
-//	hiya.FreeMessage(msg)
-//}
+import "github.com/elitecodegroovy/go-core/chart2/cgo/clib/hiya"
+
+
+func main() {
+	msg := hiya.CreateMessage("Welcome to cgo world!")
+	hiya.DisplayMessage(msg)
+	hiya.FreeMessage(msg)
+}
