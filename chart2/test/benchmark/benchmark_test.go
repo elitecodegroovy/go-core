53c7920b47b0b25d42403b7c204708e860bf2b18
elite_jigang <elite_jigang@163.com>
2018-01-02 19:50:35 +0800

    added

--- chart2/test/benchmark/benchmark_test.go
+++ chart2/test/benchmark/benchmark_test.go
@@ -3,7 +3,21 @@ package benchmark
 import (
 	"testing"
 	"bytes"
+	"fmt"
+	"strconv"
 )
+
+
+func BenchmarkSprintf(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		if x := fmt.Sprintf("%d", i); x != strconv.Itoa(i) {
+			b.Fatalf("Unexpected string: %s", x)
+		}
+	}
+	b.StopTimer()
+}
+
+
 var global string
 
 func benchmarkNBufferString(b *testing.B, numConcat int) {
