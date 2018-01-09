3e4c79af39fc8f37d79a1586781fc94076b38f4e
elite_jigang <elite_jigang@163.com>
2018-01-09 15:19:45 +0800

    added

---
+++ chart2/stdlib/strconv/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+	"log"
+)
+
+func doFormat(){
+	i := 1000
+	si := strconv.Itoa(i)
+	fmt.Printf("si : %s", si)
+	if i64, err :=strconv.ParseInt(si, 10, 32); err != nil {
+		log.Printf("error ParseInt %s", err.Error())
+	}else {
+		log.Printf("i64 value :%d", i64)
+	}
+
+	//convert2Str
+	convert2Str()
+	strconv.Quote()
+}
+
+func convert2Str(){
+	fmt.Printf("\n formatInt %s", strconv.FormatInt(100, 2))
+}
+
+
+func main(){
+	doFormat()
+}
